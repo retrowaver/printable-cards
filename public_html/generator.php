e4873d2a7299c4be4301b976c36d3d907127ba18
retrowaver <23421260+retrowaver@users.noreply.github.com>
2018-10-19 11:35:57 -0700

    web done

---
+++ public_html/generator.php
@@ -0,0 +1,19 @@
+<?php
+
+use PrintableCards\Service\PdfCardsService;
+use FPDF;
+
+define('FPDF_FONTPATH','/home/v/printable-cards/fonts');
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$data = [];
+for ($i = $_GET['start']; $i < $_GET['end']; $i++) {
+    $data[] = sprintf('%s%03d', $_GET['id'], $i);
+}
+
+$pdfCards = new PdfCardsService(new FPDF, [
+    'font_custom_ttf' => $_GET['font'],
+    'font_size' => $_GET['size']
+]);
+$pdfCards->getPrintableCards($data, $_GET['columns'], $_GET['rows']);
